0319f8d1d54d2568611f8059835b568fc4de1839
Afif Al Dzaqwan <aldzaqwan112@gmail.com>
2025-04-23 20:53:53 +0700

    update

---
+++ app/portofolio/page.tsx
@@ -0,0 +1,9 @@
+const Portofolio = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+            <h1 className="text-4xl font-bold mb-4">Portofolio</h1>
+            <p className="text-lg">Coming Soon...</p>
+        </div>
+    );
+}
+export default Portofolio;
