ad44076a91600be9610916c4db4c8cd1b418cb77
Afif Al Dzakwan <aldzaqwan112@gmail.com>
2025-04-29 20:03:24 +0700

    Update

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { div } from "three/tsl"
+
+const Contact = () => {
+    return(
+        <div>
+            <h1 className="text-white text-4xl font-bold mb-4">Contact</h1>
+            <p className="text-white text-lg">This is the Contact page.</p>
+        </div>
+    )
+}
---
+++ app/education/page.tsx
@@ -0,0 +1,8 @@
+const Education = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-[#000000] to-[#1a1a1a]">
+            <h1 className="text-white text-4xl font-bold mb-4">Education</h1>
+            <p className="text-white text-lg">This is the Education page.</p>
+        </div>
+    );
+}
